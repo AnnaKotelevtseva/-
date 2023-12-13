6f8e3fe2bd85c59e32516247032195060537e8eb
AnnaKotelevtseva <153701346+AnnaKotelevtseva@users.noreply.github.com>
2023-12-14 04:04:30 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,19 @@
 from datetime import datetime
+from typing import Self
 from app import db
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80))
     content = db.Column(db.Text)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    def __repr__(self):
-        return f"Note({self.id}, {self.title}, {self.content}, {self.created_at})"
+    created_at = db.Column(db.Date, default=datetime.utcnow)
+    
+    def _init_(self,title,content, created_at):
+        self.title=title
+        self.content=content
+        self.created_at=created_at    
+with app.app_context():
+    db.create_all()
+    db.session.add()
+    db.session.commit()
+    db.session.close()
+
