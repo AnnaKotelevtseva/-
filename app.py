ba78e00b6e94ea5087fbbf9649d6f80fabbdf40d
AnnaKotelevtseva <153701346+AnnaKotelevtseva@users.noreply.github.com>
2023-12-14 04:12:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,22 @@ from datetime import datetime
 from typing import Self
 from app import db
    
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    content = db.Column(db.Text)
+    created_at = db.Column(db.Date, default=datetime.utcnow)
+    
+    def _init_(self,title,content, created_at):
+        self.title=title
+        self.content=content
+        self.created_at=created_at    
+with app.app_context():
+    db.create_all()
+    db.session.add()
+    db.session.commit()
+    db.session.close()
+  
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -32,5 +48,7 @@ def add():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
 
 
