d54ef3b87f30304629b8e2b80b45878d62309df0
AnnaKotelevtseva <153701346+AnnaKotelevtseva@users.noreply.github.com>
2023-12-14 12:18:08 +0700

    notes.html

--- models.py
+++ models.py
@@ -1,19 +1,11 @@
-from datetime import datetime
-from typing import Self
-from app import db
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80))
-    content = db.Column(db.Text)
-    created_at = db.Column(db.Date, default=datetime.utcnow)
-    
-    def _init_(self,title,content, created_at):
-        self.title=title
-        self.content=content
-        self.created_at=created_at    
-with app.app_context():
-    db.create_all()
-    db.session.add()
-    db.session.commit()
-    db.session.close()
+<html>
+<head>
+    <title>Добро пожаловать в блокнот</title>
+</head>
+<body>
+    <h1></h1>
+    <a href="{{url_for('notes')}}">Создать заметку</a> 
+</body>
+</html>
+
 
