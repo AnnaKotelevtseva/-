cbece5a7889a81d239c56a2aca296b97121f9a5d
AnnaKotelevtseva <153701346+AnnaKotelevtseva@users.noreply.github.com>
2023-12-14 12:14:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,51 +3,55 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import select
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///zam.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
 db = SQLAlchemy(app)
 
 from datetime import datetime
 from typing import Self
 from app import db
-   
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80))
-    content = db.Column(db.Text)
-    created_at = db.Column(db.Date, default=datetime.utcnow)
-    
-    def _init_(self,title,content, created_at):
-        self.title=title
-        self.content=content
-        self.created_at=created_at    
-with app.app_context():
-    db.create_all()
-    db.session.add()
-    db.session.commit()
-    db.session.close()
-  
+
+class Note(db.Model):  # Создание модели для таблицы Zam в базе данных
+    id = db.Column(db.Integer, primary_key=True) 
+    nazvan= db.Column(db.String(50))  # Определение столбца заметка (строка, уникальное значение)
+    sod = db.Column(db.String(300))  # Определение столбца заметка (строка, уникальное значение)
+    date = db.Column(db.String(50))  # Определение столбца дата
+
+    def __init__(self, nazvan, sod, date):  # Метод для инициализации экземпляра класса Note
+        self.avtor = nazvan  # Присвоение значения заметка
+        self.sod = sod # Присвоение значения дата
+        self.date = date # Присвоение значения дата
+        
+with app.app_context():  # Вход в контекст приложения
+    db.create_all()    
+    note = Note.query.all()  # Получение всех пользователей
+    db.session.close()  # Закрытие сессии
+
+    for note in note: #вывод пользователей
+        print(f"{note.nazvan}, {note.sod},{note.date}")  
+
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/save', methods=['POST'])
 def save():
-    text = request.form['text']
-    with open('notes.txt', 'a') as file:
-        file.write(f'{text}')
-        return render_template('index.html', message='Заметка успешно сохранена')
-if __name__ == '__main__':
-    app.run(debug=True)
-    from flask import render_template
+    name=request.form['nazvan'] # Эту и последующие переменные берём из формы
+    text=request.form['sod']
+    date=request.form['date']
+    notes=Note(name,text,date) 
+    db.session.add(notes) #Добавляем  в базу данных
+    db.session.commit()
+    return render_template('index.html', message='Заметка успешно сохранена')
 
-@app.route('/add', methods=['GET', 'POST'])
-def add():
-    if request.method == 'POST':
-        return redirect('/')
-    return render_template('add.html')
+@app.route('/notes') # Определение маршрута для URL /zametkas/
+def notes(): # Определение функции для обработки запроса к URL /zametkas/
+    notes = Note.query.all() #Получение всех записей из таблицы (класса) Zametka
+    return render_template('notes.html', notes=notes) #передача списка пользователе
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
 
 
 
